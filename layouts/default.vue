aab4848be10aad3436630aefdf3b902ebc0dbb8f
chige12 <tige1215.technic@gmail.com>
2019-02-26 11:14:41 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-btn icon @click.stop="miniVariant = !miniVariant" class="text-xs-right">
+              <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
+            </v-btn>
+          </v-list-tile-action>
+        </v-list-tile>
+        <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title" />
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar :clipped-left="clipped" fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+        <v-icon>fas fa-cog</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-content>
+        <nuxt />
+    </v-content>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; Chige12 - 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: false,
+      fixed: true,
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Road Design',
+          to: '/road_design'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Minecraft Tools'
+    }
+  }
+}
+</script>
---
+++ pages/road_design.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  v-layout
+    v-list
+      v-list-tile
+
+</template>
+<script>
+export default {
+  data(){
+    return{
+      block_data:[
+        
+      ]
+    }
+  }
+}
+</script>

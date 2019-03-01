5f550348402cdc06a436b23d07a171353b56f57b
chige12 <tige1215.technic@gmail.com>
2019-03-01 22:45:16 +0900

    block_data.json

--- pages/road_design.vue
+++ pages/road_design.vue
@@ -12,6 +12,7 @@
 </template>
 <script>
 import Canvas from '~/components/Canvas.vue'
+import BlockData from '~/assets/json/block_data.json'
 
 export default {
   components: {
@@ -26,98 +27,7 @@ export default {
         {id:"swamp"},
         {id:"tundra"}
       ],
-      block_data:[
-        {
-          id:"grass_path_top",
-          name:"Grass Path",
-          jp:"小道",
-          per:"80"
-        },
-        {
-          id:"grass_plains",
-          name:"Grass",
-          jp:"草",
-          per:"10",
-        },
-        {
-          id:"coarse_dirt",
-          name:"Coarse Dirt",
-          jp:"荒土",
-          per:"10"
-        },
-        {
-          id:"cobblestone",
-          name:"Cobble Stone",
-          jp:"丸石",
-          per:"0"
-        },
-        {
-          id:"gravel",
-          name:"Gravel",
-          jp:"砂利",
-          per:"0"
-        },
-        {
-          id:"stone_bricks",
-          name:"Stone Bricks",
-          jp:"石レンガ",
-          per:"0"
-        },
-        {
-          id:"cracked_stone_bricks",
-          name:"Cracked Stone Bricks",
-          jp:"ひび割れた石レンガ",
-          per:"0"
-        },
-        {
-          id:"granite",
-          name:"Granite",
-          jp:"花崗岩",
-          per:"0"
-        },
-        {
-          id:"polished_granite",
-          name:"Polished Granite",
-          jp:"磨かれた花崗岩",
-          per:"0"
-        },
-        {
-          id:"andesite",
-          name:"Andesite",
-          jp:"安山岩",
-          per:"0"
-        },
-        {
-          id:"polished_andesite",
-          name:"Polished Andesite",
-          jp:"磨かれた安山岩",
-          per:"0"
-        },
-        {
-          id:"diorite",
-          name:"Diorite",
-          jp:"閃緑岩",
-          per:"0"
-        },
-        {
-          id:"polished_diorite",
-          name:"Polished Diorite",
-          jp:"磨かれた閃緑岩",
-          per:"0"
-        },
-        {
-          id:"mossy_cobblestone",
-          name:"Mossy Cobble Stone",
-          jp:"苔生した丸石",
-          per:"0"
-        },
-        {
-          id:"mossy_stone_bricks",
-          name:"Mossy Stone Bricks",
-          jp:"苔生した石レンガ",
-          per:"0"
-        }
-      ]
+      block_data:BlockData
     }
   }
 }
